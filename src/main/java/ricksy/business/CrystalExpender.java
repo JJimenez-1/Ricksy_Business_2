17d9b6e3a6d78d522da77262ceb724565e2b7c33
JJimenez-1 <jjimenez@cifpfbmoll.eu>
2020-02-27 12:13:41 +0100

    feat(CrystalExpender): Añadir metodo stock

--- src/main/java/ricksy/business/CrystalExpender.java
+++ src/main/java/ricksy/business/CrystalExpender.java
@@ -1,4 +1,16 @@
 package ricksy.business;
 
 public class CrystalExpender {
+
+    private int stock = 0;
+    private double itemCost = 50;
+
+    public CrystalExpender(int stock , double itemCost) {
+        this.stock = stock;
+        this.itemCost = itemCost;
+    }
+
+    public int stock() {
+        return this.stock;
+    }
 }
--- src/test/java/ricksy/business/CrystalExpenderTest.java
+++ src/test/java/ricksy/business/CrystalExpenderTest.java
@@ -1,4 +1,14 @@
 package ricksy.business;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class CrystalExpenderTest {
+
+    @Test
+    public void getStockTest() {
+        CrystalExpender crystalexpender = new CrystalExpender(3, 50);
+        assertEquals(3, crystalexpender.stock());
+    }
+
 }
